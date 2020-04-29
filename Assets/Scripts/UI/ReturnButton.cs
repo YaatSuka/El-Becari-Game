d69465dd8d1ef4aa70f68115d9cdea1eb3fb5d81
Nohan <nohan.palasse@epitech.eu>
2020-04-29 17:51:45 +0200

    Menu Screen

---
+++ Assets/Scripts/UI/ReturnButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ReturnButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Button returnButton;
+    public int scene = 0;
+
+
+	void Start() {
+		Button play = this.GetComponent<Button>();
+		play.onClick.AddListener(LoadGame);
+	}
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
---
+++ Assets/Scripts/UI/SelectLevel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SelectLevel : MonoBehaviour
+{
+    public string Path_Tag = "1";
+    public int NB_level = 0;
+    public string Desc_Level = "";
+    public bool is_Bonus = false;
+
+    GameObject NumText; 
+    GameObject LevelText;
+
+    public Sprite ImageBonus;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Path_Tag);
+        GameObject NumText = gameObject.transform.GetChild(0).gameObject;
+        GameObject LevelText = gameObject.transform.GetChild(1).gameObject;
+
+        NumText.GetComponent<Text>().text = NB_level.ToString();
+        LevelText.GetComponent<Text>().text = Desc_Level;
+
+        if (is_Bonus)
+        {
+            NumText.GetComponent<Text>().text = "";
+            gameObject.GetComponent<Image>().sprite = ImageBonus;
+        }
+
+        this.GetComponent<Button>().onClick.AddListener(LoadGame);
+    }
+
+    public void LoadGame()
+    {
+        PlayerPrefs.SetString("Path", "/Scripts/Level/Levels/level" + Path_Tag +".json");
+        SceneManager.LoadScene(2);
+    }
+}
