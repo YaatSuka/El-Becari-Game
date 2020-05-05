2d77b8ae2f485b2ea6a708583f7b19abddfe02c6
Nohan <nohan.palasse@epitech.eu>
2020-05-05 20:02:01 +0200

    path finding start

---
+++ Assets/Scripts/Player/MovementController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public float transitionSpeed = 2f; 
+    public string actualPosition = "";
+
+    
+    private Dictionary<string, Vector3> newLocation;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        newLocation = new Dictionary<string, Vector3>();
+        fillLocation();
+    }
+
+    private void FixedUpdate() {
+        if (actualPosition != "" && actualPosition != "OutLocation" && newLocation.ContainsKey(actualPosition))
+            transform.position = Vector3.Lerp(transform.position, newLocation[actualPosition], transitionSpeed * Time.deltaTime);
+        else if (actualPosition == "OutLocation")
+           transform.position = Vector3.Lerp(transform.position, newLocation[actualPosition], transitionSpeed * Time.deltaTime);
+    }
+
+    public void MoveTo(string Location)
+    {
+        actualPosition = Location;
+    }
+
+    private void fillLocation()
+    {
+        foreach(GameObject Location in GameObject.FindGameObjectsWithTag("location")) {
+            newLocation.Add(Location.name, Location.transform.position);
+         }
+    }
+}
