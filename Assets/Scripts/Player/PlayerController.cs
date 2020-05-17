ea5c407969bc21a933e8e67a152d64ddc60d016f
Nohan <nohan.palasse@epitech.eu>
2020-05-17 19:03:40 +0200

    movement ingame

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 using Interactables;
@@ -11,10 +13,47 @@ namespace Player
         public Transform box = null;
 
         private Vector3 startPosition;
+        
+        
+        public float transitionSpeed = 2f; 
+        private int path_idx = 0;
+
+        private Dictionary<string, Vector3> newLocation;
+        private Animator anim;
+        private List<string> Path;
+
 
         void Start()
         {
             this.startPosition = transform.position;
+            newLocation = new Dictionary<string, Vector3>();
+            anim = GetComponent<Animator>();
+            Path = new List<string>();
+            fillLocation();
+        }
+
+        private void FixedUpdate() {
+             if (Path.Count == 0 || Path.Count == path_idx)
+                return;
+            if (Path[path_idx] != "" && newLocation.ContainsKey(Path[path_idx]))
+            {
+                if (transform.position.x <= newLocation["OutLocationAngle"].x - 0.2 && newLocation[Path[path_idx]].x >= -0.6 ||
+                transform.position.y <= newLocation["OutLocationAngle"].y - 0.2 && newLocation[Path[path_idx]].y >= 0)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, newLocation["OutLocationAngle"], transitionSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, newLocation[Path[path_idx]], transitionSpeed * Time.deltaTime);
+                }
+                
+                if (transform.position != newLocation[Path[path_idx]])
+                    anim.SetInteger("State", 1);
+                else{
+                    anim.SetInteger("State", 0);
+                    path_idx++;
+                }
+            } 
         }
 
         public void Take()
@@ -28,6 +67,7 @@ namespace Player
                 return;
             }
 
+            MoveTo("BoxLocation");
             this.box = this.interactable.Take();
             this.box.position = new Vector2(transform.position.x, transform.position.y - 0.4f);
         }
@@ -43,6 +83,7 @@ namespace Player
                 return;
             }
 
+            MoveTo("OutLocation");
             this.interactable.Put(this.box);
             this.box = null;
         }
@@ -55,6 +96,17 @@ namespace Player
                 Destroy(this.box.gameObject);
                 this.box = null;
             }
+        }
+
+        private void MoveTo(string Location)
+        {
+            Path.Add(Location);
+        }
+
+        private void fillLocation()
+        {
+            foreach(GameObject Location in GameObject.FindGameObjectsWithTag("location"))
+                newLocation.Add(Location.name, Location.transform.position);
         }
     }
 }
